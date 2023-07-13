9a26010b2ce179badded459346fb073beb8dfd2a
ho980719 <ho980719@gmail.com>
2023-07-13 15:18:43 +0900

    todo title update

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -1,7 +1,7 @@
 import {useEffect, useState} from "react";
-import {TodoList} from "../data/TodoList";
 import {useNavigate, useParams} from "react-router-dom";
 import {request} from "../lib/request";
+import  $ from 'jquery';
 
 export const Todo = () => {
     const navigate = useNavigate();
@@ -12,7 +12,7 @@ export const Todo = () => {
 
     useEffect(() => {
         const getTodos = async () => {
-            let response = await request.get(`/todos/1/${date}`)
+            await request.get(`/todos/1/${date}`)
                 .then((response) => {
                     if (response.data.statusCode != 200)
                         throw new Error(response.data.payload.errorMessage);
@@ -21,7 +21,6 @@ export const Todo = () => {
                         setList(temp);
                     }
                 }).catch((e) => {
-                    console.error(e)
                     alert(e.message)
                 });
         }
@@ -29,41 +28,82 @@ export const Todo = () => {
     }, [])
 
 
-    // insert
-
-    // delete
-    const removeTodo = (id) => {
-        setList(list.filter(item => item.id !== id));
+    // 삭제처리
+    const removeTodo = async (id) => {
+        // 1. API 전송
+        await request.put(`/todos/delete/${id}`)
+            .then((response) => {
+                if (response.data.statusCode != 200)
+                    throw new Error(response.data.payload.errorMessage);
+                // 2.정상처리시 state 변경
+                setList(list.filter(item => item.id !== id));
+            }).catch((e) => {
+                alert(e.message)
+                return false;
+            });
     }
 
+    // 완료처리
     const updateComplete = async (id) => {
         let completed;
         list.find((item) => {
-            if(item.id === id) completed = item.completed;
+            if (item.id === id) completed = item.completed;
         });
 
         let confirmMessage = completed ? '번복하는거야?' : '완료했어?';
 
-        if (!window.confirm(confirmMessage))return false;
+        if (!window.confirm(confirmMessage)) return false;
 
-        let todo;
+        // edit 모드 일때, 화면 재 렌더링?
+        setList([...list]);
+
         // 1. API 전송
         let response = await request.put(`/todos/complete/${id}`)
             .then((response) => {
                 if (response.data.statusCode != 200)
                     throw new Error(response.data.payload.errorMessage);
                 else {
-                    todo = response.data.payload;
+                    // 2. 정상처리시 state 변경
+                    setList(
+                        list.map(item =>
+                            item.id === id ? {...item, completed: response.data.payload.completed} : item
+                        )
+                    );
                 }
             }).catch((e) => {
                 alert(e.message)
+                return false;
             });
-        // 2. 정상처리시 state 변경
-        setList(
-            list.map(item =>
-                item.id === id ? {...item, completed: todo.completed} : item
-            )
-        );
+
+    }
+
+    const updateTodo = async (el) => {
+        let id = el.dataset.id;
+        let type = el.innerText;
+        let p = el.parentElement.previousElementSibling.lastElementChild;
+        if (type == 'edit') {
+            // edit -> check
+            el.innerText = 'check';
+            // <p> -> <input>
+            $(p).html('<input type="text" class="todo-title-input" value="'+p.innerText+'"/>')
+        } else if (type == 'check') {
+            let input = p.lastElementChild;
+            let title = input.value;
+            // call api
+            await request.patch(`/todos/update`, {id: id, title: title})
+                .then((response) => {
+                    if (response.data.statusCode != 200)
+                        throw new Error(response.data.payload.errorMessage);
+                    // check -> edit
+                    el.innerText = 'edit';
+                    // <input> -> <p>
+                    $(p).html(title);
+                }).catch((e) => {
+                    alert(e.message)
+                    return false;
+                });
+        }
+        
     }
 
     return (
@@ -76,18 +116,53 @@ export const Todo = () => {
                                 return (
                                     <li key={todo.id}>
                                         <div className='todo-text-wrap'>
-                                            <input onChange={()=>updateComplete(todo.id)} checked={todo.completed && 'checked'} className="form-check-input mt-0 me-2" style={{border: '1px solid black'}} type="checkbox" value={todo.id}/>
-                                            <p>{todo.title}</p>
+                                            <input onChange={() => updateComplete(todo.id)}
+                                                   checked={todo.completed && 'checked'}
+                                                   className="form-check-input mt-0 me-2"
+                                                   type="checkbox" value={todo.id}/>
+                                            <p className={todo.completed ? 'todo-complete' : null}>{todo.title}</p>
                                         </div>
-                                        <div>
-                                            <span style={{cursor:"pointer", marginRight: 5}} className='material-symbols-outlined todo-action-btn'>edit</span>
-                                            <span onClick={()=>{removeTodo(todo.id)}} style={{cursor:"pointer"}} className='material-symbols-outlined todo-action-btn'>delete</span>
+                                        <div className='todo-action-btn-wrap'>
+                                            <span onClick={(e) => {
+                                                e.preventDefault();
+
+                                                if (todo.completed) {
+                                                    alert('완료한 데이터는 수정할수 없어..');
+                                                    return false;
+                                                }
+                                                updateTodo(e.target);
+                                            }}
+                                                  data-id={todo.id}
+                                                  className='material-symbols-outlined todo-action-btn hover-green'>edit</span>
+                                            <span onClick={(e) => {
+                                                removeTodo(todo.id);
+                                            }}
+                                                  className='material-symbols-outlined todo-action-btn hover-red'>delete</span>
                                         </div>
                                     </li>
                                 )
                             })
                             : <div>아무것도 안하게?</div>
                     }
+                    <li key='5'>
+                        <div className='todo-text-wrap'>
+                            <input onChange={() => {
+                            }}
+                                   className="form-check-input mt-0 me-2"
+                                   type="checkbox" value='5'/>
+                            <p><input type='text' className='todo-title-input' defaultValue='업데이트 테스트임'/></p>
+                        </div>
+                        <div className='todo-action-btn-wrap'>
+                            <span onClick={(e) => {
+                                e.preventDefault();
+                            }}
+                                  className='material-symbols-outlined todo-action-btn hover-green'>edit</span>
+                            <span onClick={(e) => {
+                                e.preventDefault();
+                            }}
+                                  className='material-symbols-outlined todo-action-btn hover-red'>delete</span>
+                        </div>
+                    </li>
                 </ul>
             </div>
             <div className='todo-input-box'>
