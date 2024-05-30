3070b407b92dd2ad87e1ad3740d0bb048606cb36
김준호 <ho980719@gmail.com>
2024-05-30 15:51:24 +0900

    todo view

--- src/component/TodoView.js
+++ src/component/TodoView.js
@@ -14,7 +14,7 @@ export const TodoView = () => {
                 <div className='todo-input-wrap'>
                     <button onClick={() => {
 
-                    }} type='button' className='todo-add-btn '>추가하자
+                    }} type='button' className='todo-add-btn ' style={{fontSize: '15px'}}>추가하자
                     </button>
                     {/*<input type='text' className='todo-input' placeholder='할거 입력해줘'/>*/}
                     <span className='material-symbols-outlined enter-circle'>arrow_forward</span>
